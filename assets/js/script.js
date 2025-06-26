6bb9cc94ec6d25fd73017580004fac7988825e30
VirtualEletros <papather@proton.me>
2025-06-26 17:13:00 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -517,7 +517,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Evento de submit do formulário
-    form.addEventListener('button', function(e) {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
 
         // Se estiver no passo 4 (câmera), verifique se a foto foi tirada
